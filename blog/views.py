b8d1a719fa00a25b840799a1b99efdfe9cb3f0a0
vsmrnw <vsevolod.smrnv@gmail.com>
2023-09-26 16:03:04 +0300

    add comments section to detail posts

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from django.conf import settings
-from forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from .models import Post
 
 
@@ -21,8 +22,12 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     return render(request, 'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
 
 
 def post_share(request, post_id):
@@ -43,6 +48,21 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html',
-                      {'post': post,
-                       'form': form,
-                       'sent': sent})
+                  {'post': post,
+                   'form': form,
+                   'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
