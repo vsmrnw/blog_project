a3cdb302d1406b52526e8ec627f226d78b56bcce
vsmrnw <vsevolod.smrnv@gmail.com>
2023-09-26 13:26:56 +0300

    add sharing post funtionality

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-
+from django.conf import settings
+from forms import EmailPostForm
 from .models import Post
 
 
 class PostListView(ListView):
+    """View for get all the posts"""
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
@@ -12,6 +15,7 @@ class PostListView(ListView):
 
 
 def post_detail(request, year, month, day, post):
+    """View for render detailed post by slug"""
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
                              slug=post,
                              publish__year=year,
@@ -19,3 +23,26 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
     return render(request, 'blog/post/detail.html',
                   {'post': post})
+
+
+def post_share(request, post_id):
+    """View for post share via email"""
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = (f"Read {post.title} at {post_url}\n\n {cd['name']}\'s "
+                       f"comments: {cd['comments']}")
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html',
+                      {'post': post,
+                       'form': form,
+                       'sent': sent})
