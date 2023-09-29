efb9b4098783abb081939b2385f40bd01fd073c6
vsmrnw <vsevolod.smrnv@gmail.com>
2023-09-29 14:49:43 +0300

    add tags support & most commented posts funcs

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.conf import settings
 from django.core.mail import send_mail
 from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
@@ -36,10 +37,17 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                        .order_by('-same_tags', '-publish')[:4]
     return render(request, 'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
