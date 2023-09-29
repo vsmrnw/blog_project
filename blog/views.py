3334cb23d195081f499e2a4ba5b71d00d2a48b8c
vsmrnw <vsevolod.smrnv@gmail.com>
2023-09-29 12:22:43 +0300

    refactor tags appearance in template

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,30 @@
+from django.conf import settings
 from django.core.mail import send_mail
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
-from django.views.generic import ListView
-from django.conf import settings
+from taggit.models import Tag
+
 from .forms import EmailPostForm, CommentForm
 from .models import Post
 
 
-class PostListView(ListView):
-    """View for get all the posts"""
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'posts': posts,
+                                                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -55,6 +67,7 @@ def post_share(request, post_id):
 
 @require_POST
 def post_comment(request, post_id):
+    """View for the commit post"""
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     comment = None
     form = CommentForm(data=request.POST)
