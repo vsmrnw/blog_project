5e9f3228d5e0f9136aa7e014bc809cb304d9f587
vsmrnw <vsevolod.smrnv@gmail.com>
2023-10-26 19:49:57 +0300

    added full-text search

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,9 @@ from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
-
-from .forms import EmailPostForm, CommentForm
+from django.contrib.postgres.search import SearchVector, SearchQuery, \
+    SearchRank
+from .forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post
 
 
@@ -39,9 +40,9 @@ def post_detail(request, year, month, day, post):
     form = CommentForm()
 
     post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
         .exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
                         .order_by('-same_tags', '-publish')[:4]
     return render(request, 'blog/post/detail.html',
                   {'post': post,
@@ -87,3 +88,27 @@ def post_comment(request, post_id):
                   {'post': post,
                    'form': form,
                    'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + \
+                            SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)). \
+                filter(rank__gte=0.3).order_by('-rank')
+
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
